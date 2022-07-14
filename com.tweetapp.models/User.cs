6e5b8f480a1e3306b9e5fb3e367ef67886bc39cc
Tonmoy Mukherjee <32011649+rabidlearner@users.noreply.github.com>
2022-07-14 08:44:10 -0700

    add com.tweetapp.models

---
+++ com.tweetapp.models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.tweetapp.models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
